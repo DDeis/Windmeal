70e4bfd4ff033471332be2af97d0ecf4a427743d
MLabusquiere <labusquiere@et.esiea.fr>
2013-10-07 11:19:55 +0200

    Model v1.0

---
+++ src/main/java/fr/esiea/windmeal/model/MealOrder.java
@@ -0,0 +1,49 @@
+package fr.esiea.windmeal.model;
+
+/**
+ * Copyright (c) 2013 ESIEA M. Labusquiere D. Déïs
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+public class MealOrder {
+
+    private String mealId;
+    private int number;
+
+    public String getMealId() {
+        return mealId;
+    }
+
+    public void setMealId(String mealId) {
+        this.mealId = mealId;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean setNumber(int number) {
+        if(number>0)    {
+            this.number = number;
+            return true;
+        }
+        return false;
+    }
+}
