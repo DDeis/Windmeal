b3c4c1a625a435b68cfbfd15e6bce8bd93efb6d2
DDeis <social.deis@gmail.com>
2013-10-08 16:32:59 +0200

    Added geolocalisation

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -20,6 +20,7 @@ windmealApp.config(
 		function ($routeProvider) {
 			$routeProvider.
 				when('/', {templateUrl: 'partials/home.html', controller: 'HomeController'}).
+				when('/geo', {templateUrl: 'partials/geo.html', controller: 'GeoController'}).
 
 				otherwise({redirectTo: '/'});
 		}
