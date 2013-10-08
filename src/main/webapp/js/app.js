18f4cd34e5edc21295f4848334c8900304bbf843
DDeis <social.deis@gmail.com>
2013-10-08 15:18:30 +0200

    Cleared front app code

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,24 +2,27 @@
 
 /* App Module */
 
-/*var servicesModule = angular.module('esieAddress.services', ['ngResource']);
- var controller = angular.module('esieAddress.controllers',['ngResource'])*/
 
 angular.module('windmeal.services', ['ngResource']);
 angular.module('windmeal.controllers', ['ngResource']);
 angular.module('windmeal.filters', ['ngResource']);
 angular.module('windmeal.directives', ['ngResource']);
 
-var windmealApp = angular.module('windmealApp', ['windmeal.services', 'windmeal.controllers', 'windmeal.filters', 'windmeal.directives']);
+var windmealApp = angular.module('windmealApp', [
+	'windmeal.controllers',
+	'windmeal.directives',
+	'windmeal.filters',
+	'windmeal.services'
+]);
 
-var path = "rest";
+windmealApp.config(
+	['$routeProvider',
+		function ($routeProvider) {
+			$routeProvider.
+				when('/', {templateUrl: 'partials/home.html', controller: 'HomeController'}).
 
-windmealApp.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
-	$routeProvider.
-		/* Related Contacts Pages */
-		when('/', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'}).
-
-		otherwise({redirectTo: '/'});
-
-}]);
+				otherwise({redirectTo: '/'});
+		}
+	]
+);
 
--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -4,22 +4,23 @@
 
 /* User services */
 var module = angular.module('windmeal.services');
+var path = "rest";
 
 module.factory('Login', function ($resource) {
-	return $resource('rest/login/', {}, {
+	return $resource(path+'/login/', {}, {
 		get: {method: 'GET'},
 		save: {method: 'POST'}
 	});
 });
 
 module.factory('Signup', function ($resource) {
-	return $resource('rest/users/', {}, {
+	return $resource(path+'/users/', {}, {
 		save: {method: 'POST'}
 	});
 });
 
 module.factory('Logout', function ($resource) {
-	return $resource('rest/logout/', {}, {
+	return $resource(path+'/logout/', {}, {
 		save: {method: 'POST'}
 	});
 });
