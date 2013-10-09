ee5b2b9d108117582eecd2e9ebd77131970c5bbe
DDeis <social.deis@gmail.com>
2013-10-09 19:06:39 +0200

    Added controllers

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -22,6 +22,14 @@ windmealApp.config(['$routeProvider', '$httpProvider', function ($routeProvider,
 		when('/geo', {templateUrl: 'partials/geo.html', controller: 'GeoController'}).
 		when('/signup', {templateUrl: 'partials/signup.html', controller: 'SignUpController'}).
 
+		when('/foodproviders', {templateUrl: 'partials/food-provider-list.html', controller: 'FoodProviderListController'}).
+		when('/foodproviders/:id', {templateUrl: 'partials/food-provider-detail.html', controller: 'FoodProviderDetailController'}).
+		when('/foodproviders/:id/order', {templateUrl: 'partials/order.html', controller: 'OrderController'}).
+		when('/foodproviders/:id/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'DashboardController'}).
+
+		when('/error', {templateUrl: 'partials/error.html', controller: 'ErrorController'}).
+		when('/error/:status', {templateUrl: 'partials/error.html', controller: 'ErrorController'}).
+
 		otherwise({redirectTo: '/'});
 
 	$httpProvider.responseInterceptors.push('interceptor401_403');
