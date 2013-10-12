de0e46ab87f21ae0627aacd9c8f0eb9087b2043b
MLabusquiere <labusquiere@et.esiea.fr>
2013-10-12 16:45:11 +0200

    Add rest/util/tags to get all tags

---
+++ src/main/java/fr/esiea/windmeal/controller/util/UtilCtrl.java
@@ -0,0 +1,50 @@
+package fr.esiea.windmeal.controller.util;
+
+import fr.esiea.windmeal.model.enumeration.Tag;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Copyright (c) 2013 ESIEA M. Labusquiere D. Déïs
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+@Controller
+@RequestMapping("/util")
+public class UtilCtrl {
+
+    @RequestMapping(value="/tags", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Iterable<Tag> getAll() {
+
+        List<Tag> tags = new ArrayList<Tag>();
+        for (Tag tag : Tag.class.getEnumConstants()) {
+            tags.add(tag);
+        }
+        return tags;
+    }
+}
