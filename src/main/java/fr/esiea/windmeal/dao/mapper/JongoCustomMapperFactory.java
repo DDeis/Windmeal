82c7442a410568acdabf50885a3b831d6f4cbb8b
MLabusquiere <labusquiere@et.esiea.fr>
2013-10-25 18:40:19 +0200

    Add joda module jackson to spring mvc and jongo

--- src/main/java/fr/esiea/windmeal/dao/mapper/JongoCustomMapperFactory.java
+++ src/main/java/fr/esiea/windmeal/dao/mapper/JongoCustomMapperFactory.java
@@ -1,6 +1,14 @@
-/*
- * Copyright (c) 2013. ESIEA M. Labusquiere & D. Deis
- *
+package fr.esiea.windmeal.dao.mapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.jongo.Mapper;
+import org.jongo.marshall.jackson.JacksonMapper;
+import org.springframework.stereotype.Component;
+
+/**
+ * Copyright (c) 2013 ESIEA M. Labusquiere D. Déïs
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -8,10 +16,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- *
+ * <p/>
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- *
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -20,23 +28,14 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
-package fr.esiea.windmeal.controller.mapper;
-
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-import org.springframework.stereotype.Component;
-
 @Component
-public class CustomObjectMapper extends ObjectMapper {
+public class JongoCustomMapperFactory {
+    private static Mapper mapper = new JacksonMapper.Builder()
+            .registerModule(new JodaModule())
+            .build();
 
-    public CustomObjectMapper() {
-        super();
-        this.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING,true);
-        this.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING,true);
-        this.registerModule(new JodaModule());
+    public  Mapper getMapper()  {
+        return mapper;
     }
 
 }
