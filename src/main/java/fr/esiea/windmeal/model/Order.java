9653b5fd16aa5962f1925a330b35beff768d842f
MLabusquiere <labusquiere@et.esiea.fr>
2013-10-25 18:48:01 +0200

    fix hashcode and equal

--- src/main/java/fr/esiea/windmeal/model/Order.java
+++ src/main/java/fr/esiea/windmeal/model/Order.java
@@ -65,29 +65,30 @@ public class Order extends Model {
 		this.state = state;
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		if (!super.equals(o)) return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+        if (!super.equals(o)) return false;
 
-		Order order = (Order) o;
+        Order order = (Order) o;
 
-		if (state != order.state) return false;
-		if (!foodProviderId.equals(order.foodProviderId)) return false;
-		if (!meals.equals(order.meals)) return false;
-		if (!orderDate.equals(order.orderDate)) return false;
+        if (state != order.state) return false;
+        if (foodProviderId != null ? !foodProviderId.equals(order.foodProviderId) : order.foodProviderId != null)
+            return false;
+        if (meals != null ? !meals.equals(order.meals) : order.meals != null) return false;
+        if (orderDate != null ? !orderDate.equals(order.orderDate) : order.orderDate != null) return false;
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public int hashCode() {
-		int result = super.hashCode();
-		result = 31 * result + foodProviderId.hashCode();
-		result = 31 * result + meals.hashCode();
-		result = 31 * result + orderDate.hashCode();
-		result = 31 * result + (state ? 1 : 0);
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (foodProviderId != null ? foodProviderId.hashCode() : 0);
+        result = 31 * result + (meals != null ? meals.hashCode() : 0);
+        result = 31 * result + (orderDate != null ? orderDate.hashCode() : 0);
+        result = 31 * result + (state ? 1 : 0);
+        return result;
+    }
 }
