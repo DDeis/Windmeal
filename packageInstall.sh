6f8bf8a30272d034a0337812a6d95b940343be37
MLabusquiere <labusquiere@et.esiea.fr>
2013-10-26 15:46:51 +0200

    Add packageInstall.sh

---
+++ packageInstall.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install openjdk-7-jre
+sudo apt-get install openjdk-7-jdk
+sudo apt-get install maven
+sudo apt-get install mongodb
+sudo apt-get install tomcat7
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.deb
+sudo dpkg -i elasticsearch-0.90.5.deb
