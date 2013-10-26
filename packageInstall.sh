40462db72d9886eb38a627bb6e0385702b754d9f
MLabusquiere <labusquiere@et.esiea.fr>
2013-10-26 17:07:11 +0200

    Correction

--- packageInstall.sh
+++ packageInstall.sh
@@ -1,14 +1,9 @@
-sudo apt-get update
-sudo apt-get install git
-sudo apt-get install openjdk-7-jre
-sudo apt-get install openjdk-7-jdk
-sudo apt-get install maven
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen
-sudo apt-get update 
-
-sudo apt-get install mongodb-10gen
-sudo apt-get install tomcat7
+get -y update
+sudo apt-get -y  install git
+sudo apt-get -y  install openjdk-7-jre
+sudo apt-get -y  install openjdk-7-jdk
+sudo apt-get -y  install maven
+sudo apt-get -y install mongodb
+sudo apt-get -y install tomcat7
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.deb
 sudo dpkg -i elasticsearch-0.90.5.deb
