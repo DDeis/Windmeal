8730d7e514a63ff159a5a3c664a5d354b34ae5b2
MLabusquiere <labusquiere@et.esiea.fr>
2013-10-26 21:21:05 +0200

    Add access forbidden

--- src/main/java/fr/esiea/windmeal/service/exception/NotPermitException.java
+++ src/main/java/fr/esiea/windmeal/service/exception/NotPermitException.java
@@ -28,6 +28,6 @@ import org.springframework.http.HttpStatus;
 public class NotPermitException extends ServiceException {
 
 	public NotPermitException() {
-		super(HttpStatus.UNAUTHORIZED.value(), null);
+		super(HttpStatus.FORBIDDEN.value(), null);
 	}
 }
