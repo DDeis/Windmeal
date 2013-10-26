22bc1c1505639307ea79b561d4c74521ac9faf96
MLabusquiere <labusquiere@et.esiea.fr>
2013-10-26 17:20:10 +0200

    Fix bis

--- packageInstall.sh
+++ packageInstall.sh
@@ -1,9 +1,9 @@
-get -y update
-sudo apt-get -y  install git
-sudo apt-get -y  install openjdk-7-jre
-sudo apt-get -y  install openjdk-7-jdk
-sudo apt-get -y  install maven
-sudo apt-get -y install mongodb
-sudo apt-get -y install tomcat7
+get --yes --force-yes update
+sudo apt-get --yes --force-yes  install git
+sudo apt-get --yes --force-yes  install openjdk-7-jre
+sudo apt-get --yes --force-yes  install openjdk-7-jdk
+sudo apt-get --yes --force-yes  install maven
+sudo apt-get --yes --force-yes install mongodb
+sudo apt-get --yes --force-yes install tomcat7
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.deb
 sudo dpkg -i elasticsearch-0.90.5.deb
